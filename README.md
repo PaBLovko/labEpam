2a0710a797e123f2bf7a75595daf528f93719ace
Pavel <bahmelki@gmail.com>
2021-11-30 15:22:22 +0300

    gradle

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-git task
+Kazhamiakin Pavel
---
+++ unit1/gradle/gradle-multi-project/api/src/main/java/com/epam/kazhamiakin/App.java
@@ -0,0 +1,10 @@
+package com.epam.kazhamiakin;
+
+public class App {
+
+	public static void main(String[] args) {
+		boolean result = Utils.isAllPositiveNumbers("12", "79");
+		System.out.println(result);
+	}
+
+}
