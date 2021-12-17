63f73b66a8b58156d45121af6df3e0090a8ecb75
Pavel <bahmelki@gmail.com>
2021-12-17 11:55:11 +0300

    add service

---
+++ unit2/service/src/main/java/com/epam/service/validator/GiftCertificateValidator.java
@@ -0,0 +1,56 @@
+package com.epam.service.validator;
+
+import com.epam.model.bean.GiftCertificate;
+import com.epam.model.bean.Tag;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.regex.Pattern;
+//@NoArgsConstructor(access = AccessLevel.PRIVATE);
+
+public class GiftCertificateValidator {
+    private static final Pattern NAME_PATTERN = Pattern.compile("[А-Яа-я\\w\\s\\p{Punct}]{1,256}");
+    private static final Pattern DESCRIPTION_PATTERN = Pattern.compile("[А-Яа-я\\w\\s\\p{Punct}]{1,5000}");
+
+    private GiftCertificateValidator() {
+    }
+
+    public static boolean isGiftCertificateCreationFormValid(GiftCertificate giftCertificate) {
+        return (isNameValid(giftCertificate.getName()) && isDescriptionValid(giftCertificate.getDescription()) &&
+                isPriceValid(giftCertificate.getPrice()) && isDurationValid(giftCertificate.getDuration()) &&
+                giftCertificate.getCreateDate() == null && giftCertificate.getLastUpdateDate() == null &&
+                areGiftCertificateTagsValidForCreation(giftCertificate.getTags()));
+    }
+
+    public static boolean isNameValid(String name) {
+        return (name != null && NAME_PATTERN.matcher(name).matches());
+    }
+
+    public static boolean isDescriptionValid(String description) {
+        return (description != null && DESCRIPTION_PATTERN.matcher(description).matches());
+    }
+
+    public static boolean isPriceValid(BigDecimal price) {
+        return (price != null && price.compareTo(BigDecimal.ZERO) > 0);
+    }
+
+    public static boolean isDurationValid(int duration) {
+        return duration > 0;
+    }
+
+    public static boolean areGiftCertificateTagsValidForCreation(List<Tag> tags) {
+        if (tags == null || tags.isEmpty()) {
+            return true;
+        }
+        return tags.stream().allMatch(t -> TagValidator.isNameValid(t.getName()));
+    }
+
+    public static boolean areGiftCertificateTagsValid(List<Tag> tags) {
+        if (tags == null || tags.isEmpty()) {
+            return false;
+        }
+        return tags.stream().allMatch(t -> TagValidator.isNameValid(t.getName()));
+    }
+}
---
+++ unit2/service/src/main/java/com/epam/service/validator/TagValidator.java
@@ -0,0 +1,18 @@
+package com.epam.service.validator;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Pattern;
+
+//@NoArgsConstructor(access = AccessLevel.PRIVATE);
+public class TagValidator {
+    private static final Pattern NAME_PATTERN = Pattern.compile("[А-Яа-я\\w\\s\\p{Punct}]{1,256}");
+
+    private TagValidator() {
+    }
+
+    public static boolean isNameValid(String name) {
+        return (name != null && NAME_PATTERN.matcher(name).matches());
+    }
+}
